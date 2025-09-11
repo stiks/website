f67756d5a469d67659dc65ba09feb2f959fe836c
Kirill Garbar <iterium@gmail.com>
2025-09-11 14:18:08 +0100

    feat: latest

--- src/lib/pages/home/components/connect-section.tsx
+++ src/lib/pages/home/components/connect-section.tsx
@@ -17,7 +17,7 @@ export const ConnectSection = ({ className }: ConnectSectionProps) => {
           </p>
         </div>
         <a
-          className="inline-flex items-center justify-center gap-2 whitespace-nowrap rounded-md text-sm font-medium transition-colors focus-visible:outline-none focus-visible:ring-1 focus-visible:ring-ring disabled:pointer-events-none disabled:opacity-50 [&_svg]:pointer-events-none [&_svg]:size-4 [&_svg]:shrink-0 border border-input shadow-sm hover:bg-accent hover:text-accent-foreground h-9 px-4 py-2 bg-primary text-white"
+          className="inline-flex items-center justify-center gap-2 whitespace-nowrap rounded-md text-sm font-medium transition-colors focus-visible:outline-none focus-visible:ring-1 focus-visible:ring-ring disabled:pointer-events-none disabled:opacity-50 [&_svg]:pointer-events-none [&_svg]:size-4 [&_svg]:shrink-0 border border-input shadow-sm hover:bg-accent hover:text-accent-foreground h-9 px-4 py-2 bg-primary"
           href="mailto:me@kirill.dev"
         >
           Say Hello
